661413638645a1daef8f479891925f6cee9aad08
Estarmani <estherzamani@gmail.com>
2024-02-14 00:47:40 -0800

    Bonus track

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,7 +11,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t nrd, nwr;
-	char *buff;
+	char *buf;
 
 	if (!filename)
 		return (0);
@@ -25,12 +25,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!buf)
 		return (0);
 
-	nrd = read(fd, buff, letters);
-	nwr = write(STDOUT_FILENO, buff, nrd);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
 	close(fd);
 
-	free(buff);
+	free(buf);
 
 	return (nwr);
+}
 }
